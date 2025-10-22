8039c21d75acb61a9315069bc704be84b642cc0e
HugoDelgadoo <hugodelgadofernandez@gmail.com>
2025-10-22 10:17:13 +0200

    primer commit

---
+++ ejercicios.py
@@ -0,0 +1,22 @@
+#PY0016 a
+for i in range(21):
+    print(i)
+
+#PY0016 b
+for i in range(21):
+    if(i % 2 == 0):
+        print(i)
+
+
+#PY0016 c
+for i in range(21):
+    if(i % 2 != 0):
+        print(i)
+
+
+#PY0017
+resultado = sum(range(101))
+print(resultado)
+
+
+
